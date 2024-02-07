45edcd8f8ebb3cc72727ae25b03f85ff0a2fc2b6
Om Jadhav <omjadhav.github@gmail.com>
2024-02-08 00:59:12 +0530

    Initializing Project

---
+++ portfolio-site/app/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeroSection = () =>{
+    return (
+        <section>
+            <h1 className="text-white mb-4 text-6xl font-extrabold">
+                Hello, I'm Om
+            </h1>
+            <p className="text-[#ADB7BE]"> 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero similique molestias odio obcaecati voluptatum, a recusandae amet iure minima voluptates explicabo fugiat consectetur dolorem, eveniet repudiandae quam natus, unde reiciendis.
+            </p>
+        </section>
+    )
+}
+
+export default HeroSection;
---
+++ portfolio-site/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+import { Main } from "next/document";
+import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
+      <HeroSection />
+    </main>
+  );
+}
